a6bc61b8df2b693e5f7707680196e77c1f1df5bb
JohnEko <zino@DESKTOP-QOOLFO0.>
2025-05-10 10:37:00 +0100

    mobile navbar

--- app/components/mobile-nav.tsx
+++ app/components/mobile-nav.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { Button } from '@/components/ui/button';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 import { Menu } from 'lucide-react';
 import Link from "next/link";
 import { LinkProps } from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
+import { Icons } from './icons';
+import { siteConfig } from '@/config/site';
 
 
 export default function MobileNav(){
@@ -17,10 +19,31 @@ export default function MobileNav(){
         <SheetTrigger asChild>
             <Button variant="outline" className="w-10 px-0 sm:hidden">
                 <Menu className='h5 w-5' />
-                <span className='sr-only'>Toggle Theme</span>
+                <SheetTitle className='sr-only'>Toggle Theme</SheetTitle>
             </Button>
         </SheetTrigger>
-        <SheetContent side='right'></SheetContent>
+        <SheetContent side='right'>
+            {/* adding content when the app is used on mobilelink
+            when the app is on mobile mode this function represent mobile mode */}
+            <MobileLink onOpenChenge={setOpen} href="/" className="flex items-center">
+                <Icons.logo className='mr-2 h-4 w-4' />
+                <span className='font-bold'>{siteConfig.name}</span>
+            </MobileLink>
+            <div className='flex flex-col gap-3 mt-3'>
+                <MobileLink onOpenChenge={setOpen} href="/blog">
+                    Blog
+                </MobileLink>
+                <MobileLink onOpenChenge={setOpen} href="/about">
+                    About
+                </MobileLink>
+                <Link target='_blank' rel='noreferrer' href={siteConfig.links.github}>
+                    Github
+                </Link>
+                <Link target='_blank' rel='noreferrer' href={siteConfig.links.twitter}>
+                    Twitter
+                </Link>
+            </div>
+        </SheetContent>
     </Sheet>
   );
 }
@@ -28,24 +51,28 @@ export default function MobileNav(){
 interface MobileLinkProps extends LinkProps {
     children: React.ReactNode;
     onOpenChenge?: (open: boolean) => void;
+    className?: string;
 }
 
 function MobileLink({
     href,
     onOpenChenge,
     children,
+    className,
     ...props
 }: MobileLinkProps) {
-    const router = useRouter()
-    return <Link
+    const router = useRouter();
+    return (
+            <Link
                 href={href}
                 onClick={() => {
                     router.push(href.toString());
                     onOpenChenge?.(false);
                 }}
+                className="className"
                 {...props}
                 >
                 {children}
             </Link>
-            
+    );  
 }
